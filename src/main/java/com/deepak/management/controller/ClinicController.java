fb1fbdb6ddfe1001586a1e0d58576636a07c1b7c
Deepak Sekarbabu <deepakinmail@gmail.com>
2024-01-04 09:00:04 +0100

    Updates

--- src/main/java/com/deepak/management/controller/ClinicController.java
+++ src/main/java/com/deepak/management/controller/ClinicController.java
@@ -7,7 +7,6 @@ import com.deepak.management.service.ClinicService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
