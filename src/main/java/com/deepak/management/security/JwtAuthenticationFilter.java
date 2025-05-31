76b70c7eae24b6b514946060bea5b978be1ae406
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-05-31 23:00:29 +0200

    Refactor QueueManagementRepository to replace user details retrieval with patient details; update SQL queries for improved data handling. Clean up imports in JwtAuthenticationFilter for better readability.

--- src/main/java/com/deepak/management/security/JwtAuthenticationFilter.java
+++ src/main/java/com/deepak/management/security/JwtAuthenticationFilter.java
@@ -1,18 +1,16 @@
 package com.deepak.management.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
