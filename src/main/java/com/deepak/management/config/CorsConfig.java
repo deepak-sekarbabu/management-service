2f0951a91cf8892d81338144eb4cea87d8f95704
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-06-21 18:49:04 +0200

    Format CORS configuration code for better readability
    This commit reformats the code in CorsConfig.java to improve readability by adjusting indentation and spacing. No functional changes were made to the CORS configuration logic.

--- src/main/java/com/deepak/management/config/CorsConfig.java
+++ src/main/java/com/deepak/management/config/CorsConfig.java
@@ -9,16 +9,16 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("http://localhost:3030");
-        config.addAllowedOriginPattern("https://management-ui-pdj4.onrender.com");
-        config.addAllowedMethod("*"); // You can configure specific HTTP methods
-        config.addAllowedHeader("*"); // You can configure specific headers
-        config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedOriginPattern("http://localhost:3030");
+    config.addAllowedOriginPattern("https://management-ui-pdj4.onrender.com");
+    config.addAllowedMethod("*"); // You can configure specific HTTP methods
+    config.addAllowedHeader("*"); // You can configure specific headers
+    config.setAllowCredentials(true);
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
 }
