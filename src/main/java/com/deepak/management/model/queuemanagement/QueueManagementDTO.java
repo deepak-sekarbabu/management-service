b7671fe781cee7275dbb40700a6d45f1f54b6949
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-06-22 21:32:28 +0200

    Add NoArgsConstructor to QueueManagementDTO and update shiftTime format
    This commit adds the @NoArgsConstructor annotation to QueueManagementDTO for better object initialization. It also updates the shiftTime field in QueueManagementRepository queries to remove TIME_FORMAT, likely to return the raw value instead of a formatted string, ensuring consistency in data handling.

--- src/main/java/com/deepak/management/model/queuemanagement/QueueManagementDTO.java
+++ src/main/java/com/deepak/management/model/queuemanagement/QueueManagementDTO.java
@@ -1,12 +1,14 @@
 package com.deepak.management.model.queuemanagement;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class QueueManagementDTO {
   private Integer id;
   private String avatarUrl;
